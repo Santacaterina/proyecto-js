cc2c75b08fa1c71f17214ca3baa1add408db37ac
Alfredo Ruiz S. <alfredoruizsa@gmail.com>
2023-12-25 14:39:29 -0400

    ultima modificacion

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const fn = () => {
     const miString = "soy un string!"
     let b = miString + " muy feliz"
     console.log(b)
+    console.log("hola mundo")
+    console.console.log('56');
+    
+
+
+    
 }
 
 fn()
